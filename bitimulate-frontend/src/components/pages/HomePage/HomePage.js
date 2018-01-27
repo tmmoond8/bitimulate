b640c191017622a5fee3225bda0974bd8651371a
Taemin Moon <tmmoond8@gmail.com>
2018-01-28 01:27:08 +0900

    ep7 - 헤더에 네비게이션 작업 - 나눔 스퀘어 폰트 추가 - 랜딩에 대문 만듬

--- bitimulate-frontend/src/components/pages/HomePage/HomePage.js
+++ bitimulate-frontend/src/components/pages/HomePage/HomePage.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Header, PageTemplate, PolyBackground } from 'components';
+import { Header, PageTemplate, PolyBackground, Block } from 'components';
 
 const HomePage = () => {
+  const style = {
+    color: 'black'
+  }
   return (
     <PageTemplate 
       header={<Header/>}>
-      <PolyBackground/>
-      Home
+      <PolyBackground>
+        <Block style={style} center shadow>
+          <h1>
+            모의 거래소에서 다양한 가상화페 거래를 진행해보세요.
+          </h1>
+          <h2>
+            실제 거래소의 실시간 데이터에 기반하여 거래를 진행합니다.
+          </h2>
+        </Block>
+      </PolyBackground>
     </PageTemplate>
   );
 };
