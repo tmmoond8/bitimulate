72381ef35f289b090f2a1571ba8683f1fb5eedd5
Taemin Moon <tmmoond8@gmail.com>
2018-02-04 21:58:10 +0900

    ep12 - 로그인 모달 디자인 완성

--- bitimulate-frontend/src/components/organisms/LoginModal/LoginModal.stories.js
+++ bitimulate-frontend/src/components/organisms/LoginModal/LoginModal.stories.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import LoginModal from './LoginModal';
-
+const forms = {
+  login: {
+    email: "", 
+    password: ""
+  },
+  register: {
+    email: "", 
+    password: "", 
+    dispalyName: ""
+  }
+}
 storiesOf('LoginModal', module)
   .add('default', () => (
-    <LoginModal></LoginModal>))
+    <LoginModal 
+      visible
+      forms={forms}
+    ></LoginModal>))
--- bitimulate-frontend/src/containers/LoginModalContainer.js
+++ bitimulate-frontend/src/containers/LoginModalContainer.js
@@ -2,21 +2,81 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { LoginModal } from 'components';
+import onClickOutside from 'react-onclickoutside';
+import * as baseActions from 'store/modules/base';
+import * as authActions from 'store/modules/auth';
 
 class LoginModalContainer extends Component {
+  handleClickOutside = (event) => {
+    const { visible, BaseActions, AuthActions } = this.props;
+    if (!visible) return;
+    BaseActions.setScreenMaskVisiblity(false);
+    AuthActions.toggleLoginModal();
+  }
+  handleChangeMode = () => {
+    const { mode, AuthActions } = this.props;
+    const inverted = mode === 'login' ? 'register' : 'login';
+    AuthActions.setModalMode(inverted);
+  }
+  handleChangeInput = (event) => {
+    const { AuthActions } = this.props;
+    const { name, value } = event.target;
+
+    AuthActions.handleChangeInput({
+      name,
+      value
+    });
+  }
+  handleLogin = () => {
+    console.log('handleLogin');
+  }
+
+  handleRegister = () => {
+    // validate email and password
+    const constraints = {
+      email: {
+        email: {
+          message: () => `잘못된 형식의 이메일 입니다.`
+        }
+      },
+      password: {
+        length: {
+          minimum: 6,
+          tooShort: `비밀번호는 %{count}자 이상 입력하세요.`
+        }
+      }
+    }
+    const forms = this.props.forms.toJS();
+    const { AuthActions } = this.props;
+  }
   render() {
-    const  { visible } = this.props;
+    const  { visible, mode, forms } = this.props;
+    const { 
+      handleChangeMode, 
+      handleChangeInput, 
+      handleLogin, 
+      handleRegister 
+    } = this;
+
     return (
-      <LoginModal visible={visible}/>
+      <LoginModal
+        visible={visible}
+        mode={mode}
+        forms={forms}
+        onChangeInput={handleChangeInput}
+        onChangeMode={handleChangeMode}/>
     )
   }
 }
 
 export default connect(
   (state) => ({
-    visible: state.base.getIn(['screenMask', 'visible'])
+    visible: state.auth.getIn(['modal', 'visible']),
+    mode: state.auth.getIn(['modal', 'mode']),
+    forms: state.auth.get('forms'),
   }),
   (dispatch) => ({
-    
+    BaseActions: bindActionCreators(baseActions, dispatch),
+    AuthActions: bindActionCreators(authActions, dispatch)
   })
-)(LoginModalContainer);
+)(onClickOutside(LoginModalContainer));
